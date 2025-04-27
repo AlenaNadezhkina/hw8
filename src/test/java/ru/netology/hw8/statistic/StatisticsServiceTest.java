e82824c10593e18ad554dedf46998cfd9cde90a6
AlenaNadezhkina <61390359+AlenaNadezhkina@users.noreply.github.com>
2025-04-27 16:40:35 +0300

    New commit

--- src/test/java/ru/netology/hw8/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/hw8/statistic/StatisticsServiceTest.java
@@ -1,4 +1,5 @@
 package ru.netology.hw8.statistic;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -10,6 +11,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findNum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 50, 3, 8, 6, 11, 11, 12};
+        long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
 
